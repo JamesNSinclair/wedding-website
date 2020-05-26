1272fa387f5b37225944b13a8db46636e485313f
JamesNSinclair <53437429+JamesNSinclair@users.noreply.github.com>
2020-05-26 13:33:37 +0100

    some updates

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-const question = document.getElementById("question");
+let question = document.querySelectorAll("#question");
+let answer = document.querySelectorAll('#answer')
+const hotel-modal = document.querySelectorAll('#hotel-modal')
+
+for (let i=0; 1 < question.length; i+=1 ) {
+    let questions = question[i]
+    let answers = answer[i]
+
+questions.addEventListener('click', () => {
+  if (answers.style.visibility === "visible") {
+      answers.style.transform = "translateY(-5px)";
+    answers.classList.toggle("height");
+    answers.style.visibility = "hidden";
 
 
 
-question.addEventListener('click', () => {
-  let answer = document.getElementById('answer')
-  console.log('answer');
+  } else {
+    answers.style.visibility = "visible";
+      answers.style.transform = "translateY(10px)";
+      answers.classList.toggle("height");
 
-
-
-  if (answer.style.display === "none") {
-    answer.style.display = "show";
-     console.log('hello');
-  } else { answer.style.display = "none";
-   console.log('goodbye');;
-}
+    }
 
 });
+}
+
+
+<div id='hotel-modal' class="modal-body">
+  <img src="images/harbour-hotel.jpg" class="hotel">
+  <h2 class='pt-1'> This is the Fowley Hotel </h2>
+  <p> 5 miles from Eden Project </p>
+  <p> 4 stars trip adviser </p>
+    <p> Here is sosme tejes psab otehsi aoweojewg jds sdjer j sododjw jwewewej owwj wjo wejfweovewwnfxan hgrd sdwfweffw </p>
+</div>
