2e6deb2b33fd604165923f67bacdbb4240585b7b
JamesNSinclair <53437429+JamesNSinclair@users.noreply.github.com>
2020-05-26 17:05:04 +0100

    can't get modal to work

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let question = document.querySelectorAll("#question");
-let answer = document.querySelectorAll('#answer')
-const hotel-modal = document.querySelectorAll('#hotel-modal')
+let answer = document.querySelectorAll('#answer');
+const hotelModal = document.querySelectorAll('#hotel-modal');
+const hotels = document.querySelectorAll('.hotels');
+let modal = document.querySelector('.modal-content');
+const button2 = document.querySelector('#button-2');
+const button1 = document.querySelector('#button-1');
 
-for (let i=0; 1 < question.length; i+=1 ) {
-    let questions = question[i]
-    let answers = answer[i]
+for (let i=0; i < question.length; i+=1 ) {
+    let questions = question[i];
+    let answers = answer[i];
 
-questions.addEventListener('click', () => {
+questions.addEventListener('click', (e) => {
   if (answers.style.visibility === "visible") {
       answers.style.transform = "translateY(-5px)";
     answers.classList.toggle("height");
@@ -25,10 +29,50 @@ questions.addEventListener('click', () => {
 }
 
 
-<div id='hotel-modal' class="modal-body">
-  <img src="images/harbour-hotel.jpg" class="hotel">
-  <h2 class='pt-1'> This is the Fowley Hotel </h2>
-  <p> 5 miles from Eden Project </p>
-  <p> 4 stars trip adviser </p>
-    <p> Here is sosme tejes psab otehsi aoweojewg jds sdjer j sododjw jwewewej owwj wjo wejfweovewwnfxan hgrd sdwfweffw </p>
-</div>
+button1.addEventListener('click', () => {
+  console.log('yo');
+  createScroll(0);
+});
+
+
+
+function createScroll(index) {
+
+	let next = document.querySelector('.next');
+	let previous = document.querySelector('.previous');
+
+
+		next.addEventListener('click', (e) => {
+
+			if (index < 7) {
+					index += 1;
+          console.log(index);
+				 createModal(index);
+				 		} else {
+				 console.log('next')
+				}
+		});
+
+		previous.addEventListener('click', e => {
+			if (index > 0) {
+					index += -1;
+          console.log(index);
+		 			createModal(index);
+			  } else {
+				console.log('previous')
+			}
+		});
+
+}
+
+
+function createModal(index) {
+ let changeHotel = hotels[index];
+	console.log(changeHotel);
+
+  const html = changeHotel;
+
+modal.innerHTML = html.innerHTML;
+
+
+}
